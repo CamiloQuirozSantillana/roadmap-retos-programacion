4c00a58395427dcce0dd92d490bc08cf388c70bc
CamiloQuirozSantillana <juancamiloquirozsantillana@gmail.com>
2024-11-06 23:22:22 -0500

    Create CamiloQuirozSantillana.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/CamiloQuirozSantillana.py
@@ -0,0 +1,26 @@
+# https://python.org
+
+# Esto es un comentario en una sola linea 
+
+"""
+esto es un comentaraio
+en varias
+lineas 
+"""
+
+'''
+y esto tambien 
+es un comentario 
+en phyton 
+'''
+
+nombre = "Camilo"
+MY_CONSTANT = "Mi constante"
+
+My_int = 1
+My_float = 1.4
+My_bool = True
+My_bool= False
+My_str = "mi cadena de texto"
+
+print ("hola, phyton")
